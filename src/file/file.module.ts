472eb26427c8786fe3bcf38752ff37b547acf4de
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2025-05-08 08:16:09 +0700

    add file entity module

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileEntity, FileSchema } from '../schemas/file.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: FileEntity.name, schema: FileSchema }]),
+  ],
   controllers: [FileController],
   providers: [FileService],
 })
